3e2d0b701278e522ec2ca3c4f4b137fbfad2b0f5
ZuzannaFilipkowska <zuziafilipkowska@gmail.com>
2023-06-04 16:44:10 +0200

    Added adoption steps

---
+++ app/components/cases/StepThreeForm.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const StepOneForm: React.FC<{
+}> = (props) => {
+  return (
+    <div>
+      <p className="text-lg font-semibold mb-1">Szczegóły etapu 3</p>
+      <div className="flex mb-4">
+        <div className="flex flex-col">
+          <label htmlFor="stepOneDate" className="text-lg font-semibold mb-1"> Data wizyty </label>
+          <input
+              type="date"
+              id="stepOneDate"
+              placeholder=""
+              className="w-72 rounded-md border-gray-200 py-2.5 pe-10 shadow-sm sm:text-sm"
+          />
+        </div>
+        <div className="flex flex-col ml-10 justify-content">
+          <label htmlFor="stepOneFinish" className="text-lg font-semibold mb-1"> Czy chcesz zakończyć etap?</label>
+          <input
+              type="checkbox"
+              id="stepOneFinish"
+              placeholder=""
+              className="h-5 rounded-md border-gray-200 shadow-sm sm:text-sm mt-3"
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col">
+        <label htmlFor="stepOneComment" className="text-lg font-semibold mb-1"> Uwagi do przebiegu etapu </label>
+        <textarea
+            id="stepOneComment"
+            placeholder="Opisz przebieg etapu..."
+            className="w-full rounded-md border-gray-200 py-2.5 pe-10 shadow-sm sm:text-sm"
+        />
+      </div>
+
+    </div>
+  );
+};
+
+export default StepOneForm;
--- app/pages/cases/[id]/index.tsx
+++ app/pages/cases/[id]/index.tsx
@@ -4,6 +4,11 @@ import {useEffect, useState} from "react";
 import {Adoption} from "../../api/cases/[id]";
 import Page from "@/components/page/Page";
 import Button from "@/components/Button";
+import localFont from "@next/font/local";
+import StepOneForm from "@/components/cases/StepOneForm";
+import StepTwoForm from "@/components/cases/StepTwoForm";
+import StepThreeForm from "@/components/cases/StepThreeForm";
+import StepFourForm from "@/components/cases/StepFourForm";
 
 type Props = {};
 
@@ -13,6 +18,9 @@ const AdoptionDetailsPage:  NextPage<Props> = (props) => {
   const id = router.query.id as string
 
   const [adoptionDetails, setAdoptionDetails] = useState({} as unknown as Adoption);
+  const [formStep, setFormStep] = useState(1);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
   useEffect(() => {
     async function getData() {
@@ -29,6 +37,24 @@ const AdoptionDetailsPage:  NextPage<Props> = (props) => {
   }, [id])
 
 
+  const changeStep = (data) => {
+    setFormStep(data)
+  }
+  
+  function CurrentStepForm(num: number) {
+    switch(num) {
+      case 1:
+        return <StepOneForm/>;
+      case 2:
+        return <StepTwoForm/>;
+      case 3:
+        return <StepThreeForm/>;
+      case 4: 
+        return <StepFourForm/>;
+      default:
+        return <StepOneForm/>;
+    }
+  }
   return (
       <>
         {/*@ts-ignore*/}
@@ -85,36 +111,48 @@ const AdoptionDetailsPage:  NextPage<Props> = (props) => {
             </div>
             <div className="ml-6 bg-slate-200 rounded-lg pl-4 pr-8 py-4">
               <p className="text-2xl font-medium mt-2 mb-2">Etapy adopcji</p>
-              <form // onSubmit={infoChangeHandler}
-                  className="flex flex-col w-full md:h-[25%] xl:h-[35%]">
                 <div
                     className="relative after:absolute after:inset-x-0 after:top-1/2 after:block after:h-0.5 after:-translate-y-1/2 after:rounded-lg after:bg-gray-100"
                 >
                   <ol className="relative z-10 flex justify-between text-sm font-medium text-gray-500">
-                    <li className="flex items-center gap-2 bg-white p-2">
-                      <span className="h-6 w-6 rounded-full bg-gray-100 text-center text-[10px]/6 font-bold">
+                    <li className="flex items-center gap-2 bg-white p-2" onClick={() => changeStep(1)}>
+                      <span className={`h-6 w-6 rounded-full text-center text-[10px]/6 font-bold ${formStep === 1 ? "bg-blue-600 text-white" : "bg-gray-100"}`}>
                         1
                       </span>
-                      <span className="hidden sm:block"> Details </span>
+                      <span className="hidden sm:block"> Rozmowa i ankieta </span>
                     </li>
 
-                    <li className="flex items-center gap-2 bg-white p-2">
-                      <span className="h-6 w-6 rounded-full bg-blue-600 text-center text-[10px]/6 font-bold text-white">
+                    <li className="flex items-center gap-2 bg-white p-2" onClick={() => changeStep(2)}>
+                      <span className={`h-6 w-6 rounded-full text-center text-[10px]/6 font-bold ${formStep === 2 ? "bg-blue-600 text-white" : "bg-gray-100"}`}>
                         2
                       </span>
-                      <span className="hidden sm:block"> Address </span>
+                      <span className="hidden sm:block"> Zapoznanie w schronisku</span>
                     </li>
-                    <li className="flex items-center gap-2 bg-white p-2">
-                      <span className="h-6 w-6 rounded-full bg-gray-100 text-center text-[10px]/6 font-bold">
+                    <li className="flex items-center gap-2 bg-white p-2" onClick={() => changeStep(3)}>
+                      <span className={`h-6 w-6 rounded-full text-center text-[10px]/6 font-bold ${formStep === 3 ? "bg-blue-600 text-white" : "bg-gray-100"}`}>
                         3
                       </span>
-                      <span className="hidden sm:block"> Payment </span>
+                      <span className="hidden sm:block"> Wizyta przedadopcyjna </span>
+                    </li>
+                    <li className="flex items-center gap-2 bg-white p-2" onClick={() => changeStep(4)}>
+                      <span className={`h-6 w-6 rounded-full text-center text-[10px]/6 font-bold ${formStep === 4 ? "bg-blue-600 text-white" : "bg-gray-100"}`}>
+                        4
+                      </span>
+                      <span className="hidden sm:block"> Podpisanie umowy </span>
                     </li>
                   </ol>
                 </div>
+            </div>
+
+            <div className="ml-6 bg-slate-200 rounded-lg pl-4 pr-8 py-4 mt-2">
+
+              <form // onSubmit={infoChangeHandler}
+                  className="flex flex-col w-full md:h-[25%] xl:h-[35%]">
+                {CurrentStepForm(formStep)}
 
               </form>
             </div>
+
           </div>
         }/>
       </>
