73d2a7c3de3a769cd4aeb823a3f3b73bf9906c8e
MariuszPaluch2001 <mariuszpaluch001@gmail.com>
2023-06-04 16:10:46 +0200

    Add new message page look.

--- app/pages/messages/[id]/index.tsx
+++ app/pages/messages/[id]/index.tsx
@@ -2,6 +2,7 @@ import {useRouter} from "next/router";
 import {useEffect, useState} from "react";
 
 import Page from "@/components/page/Page";
+import Link from "next/link";
 
 type WorkerInfo = {
   firstName: string,
@@ -48,87 +49,64 @@ export default function MessagePage() {
         {/*@ts-ignore*/}
         <Page children={
           <div>
-            <h2>Szczegóły zgłoszenia</h2>
-            <section>
-              <div className="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
-                <div
-                  className="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-16"
-                >
-                  <div
-                    className="mx-auto max-w-lg text-center lg:mx-0 ltr:lg:text-left rtl:lg:text-right"
-                  >
-                    <h2 className="text-3xl font-bold sm:text-4xl">Załączona wiadomość</h2>
+          <h2>Szczegóły zgłoszenia</h2>
+          <br/>
+          <div className="w-max">
+            <div className="ml-6 bg-slate-200 rounded-lg pl-4 pr-8 py-4">
+              <div
+                className="mx-auto max-w-lg text-center lg:mx-0 ltr:lg:text-left rtl:lg:text-right"
+              >
+                <h2 className="text-3xl font-bold sm:text-4xl">Załączona wiadomość</h2>
             
-                    <p className="mt-4 text-gray-600">
-                        {messageDetails.message}
-                    </p>
-                  </div>
-            
-                  <div className="grid grid-cols-2 gap-4 sm:grid-cols-3">
-                    <a
-                      className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
-                    >
-                      <h5 className="mt-2 font-bold">Dane klienta</h5>
-            
-                      <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
-                        {messageDetails.candidateFirstName} {messageDetails.candidateLastName}
-                      </p>
-                    </a>  
-                    <a
-                      className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
-                    >
-            
-                      <h5 className="mt-2 font-bold">Wybrane zwierzę</h5>
-            
-                      <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
-                        {messageDetails.petName}
-                      </p>
-                    </a>
-            
-                    <a
-                      className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
-                    >
-            
-                      <h5 className="mt-2 font-bold">Data zgłoszenia</h5>
-            
-                      <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
-                        {messageDetails.date?.toString().substring(0,10)}
-                      </p>
-                    </a>
-            
-                    <a
-                      className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
-                    >
-            
-                      <h5 className="mt-2 font-bold">Osoba przypisana</h5>
-            
-                      <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
-                        {messageDetails.worker?.firstName} {messageDetails.worker?.lastName}
-                      </p>
-                    </a>
-                    <a
-                      className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
-                    >
-                      <h5 className="mt-2 font-bold">Dane kontaktowe</h5>
-            
-                      <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
-                        {messageDetails.candidateContactInfo}
-                      </p>
-                    </a>
-                  </div>
-                </div>
+                <p className="mt-4 text-gray-600">
+                    {messageDetails.message}
+                </p>
               </div>
-            </section>
-            <div className="float-right">
+            </div>
+            <p className="text-2xl font-medium ml-6 mt-8">Dodatkowe informacje</p>
+            <br/>
+            <div className="ml-6 bg-slate-200 rounded-lg pl-4 pr-8 py-4">
+
+              <div className="">
+                <label className="block text-xl font-soft text-gray-700">
+                  Dane klienta: {messageDetails.candidateFirstName} {messageDetails.candidateLastName}
+                </label>
+              </div>
+              <div className="mt-4">
+                <label className="block text-xl font-soft text-gray-700">
+                  Wybrane zwierzę: {messageDetails.petName}
+                </label>
+              </div>
+              <div className="mt-4">
+                <label className="block text-xl font-soft text-gray-700">
+                Data zgłoszenia: {messageDetails.date?.toString().substring(0,10)}
+                </label>
+              </div>
+              <div className="mt-4">
+                <label className="block text-xl font-soft text-gray-700">
+                Przypisany pracownik: {messageDetails.worker?.firstName} {messageDetails.worker?.lastName}
+                </label>
+              </div>
+              <div className="mt-4">
+                <label className="block text-xl font-soft text-gray-700">
+                Dane kontaktowe: {messageDetails.candidateContactInfo}
+                </label>
+              </div>
+            </div>
+          </div>
+          <br/>
+            <div className="">
 
               <span
-                  className="inline-flex -space-x-px overflow-hidden rounded-md border bg-[#F4694D] shadow-sm mx-10"
+                  className="inline-flex -space-x-px overflow-hidden rounded-md border bg-[#F4694D] shadow-sm mx-6"
                 >
-                <button
-                  className="inline-block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:relative"
-                >
-                  Powrót
-                </button>
+                <Link href={"/messages"}>
+                  <button
+                    className="inline-block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:relative"
+                  >
+                    Powrót
+                  </button>
+                </Link>
 
                 <button
                   className="inline-block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:relative"
@@ -136,11 +114,11 @@ export default function MessagePage() {
                   Zamknij zgłoszenie
                 </button>
 
-                <button
+                {/* <button
                   className="inline-block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:relative"
                 >
                   Utwórz sprawę adopcyjną
-                </button>
+                </button> */}
               </span>
             </div>
           </div>
