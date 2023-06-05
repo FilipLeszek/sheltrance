de26a0ee711c276875526e48437a2ca99a1c167f
Michał Kowalczyk <mpkowalcz@wp.pl>
2023-06-05 03:54:35 +0200

    Minor cosmetic changes

--- app/pages/shelter/workers/index.tsx
+++ app/pages/shelter/workers/index.tsx
@@ -318,7 +318,7 @@ const ShelterEmployeesPage:  NextPage<Props> = (props) => {
                         viewBox="0 0 24 24" 
                         strokeWidth={1.5} 
                         stroke="currentColor"
-                        className="w-8 h-8 group-hover:hidden">
+                        className="w-7 h-7 group-hover:hidden">
                         <path strokeLinecap="round" 
                         strokeLinejoin="round"
                         d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
@@ -330,7 +330,7 @@ const ShelterEmployeesPage:  NextPage<Props> = (props) => {
                         viewBox="0 0 24 24" 
                         strokeWidth={1.5} 
                         stroke="currentColor"
-                        className="w-8 h-8 hidden group-hover:block">
+                        className="w-7 h-7 hidden group-hover:block">
                         <path 
                           strokeLinecap="round" 
                           strokeLinejoin="round" 
@@ -362,7 +362,7 @@ const ShelterEmployeesPage:  NextPage<Props> = (props) => {
                       xmlns="http://www.w3.org/2000/svg" 
                       fill="none" 
                       viewBox="0 0 24 24" 
-                      strokeWidth="1.0" 
+                      strokeWidth="1.2" 
                       stroke="black" 
                       className="w-8 h-8">
 
